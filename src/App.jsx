d69fa36923b062fa3fd37bae89bda7fd44498922
md-jihad-khan <mdjihadkhan9867@gmail.com>
2024-03-23 15:16:09 +0600

    implement the light and dark theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
+import Nav from "./components/Nav";
+import Blogs from "./pages/Blogs";
+import Bookmarks from "./pages/Bookmarks";
+import Home from "./pages/Home";
+
 function App() {
   return (
     <>
-      <h1>hello</h1>
+      <div className="h-16">
+        <Nav></Nav>
+      </div>
+      <Home></Home>
+      <Blogs></Blogs>
+      <Bookmarks></Bookmarks>
     </>
   );
 }
